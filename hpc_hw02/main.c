6b80a9b8b75bbcbb91de87459cb325dd7fef654b
Ehsan Valizadeh <ehsan@MASL-02.local>
2014-09-29 17:31:59 -0400

    Initial Commit

---
+++ hpc_hw02/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  hpc_hw02
+//
+//  Created by Ehsan Valizadeh on 9/29/14.
+//  Copyright (c) 2014 Ehsan Valizadeh. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
