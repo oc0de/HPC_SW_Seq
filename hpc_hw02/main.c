b6acc12afe43fd6b623eccbe0c577c881aa54b4d
Ehsan Valizadeh <ehsan@MASL-02.local>
2014-09-29 18:39:41 -0400

    Read from a file

--- hpc_hw02/main.c
+++ hpc_hw02/main.c
@@ -6,13 +6,38 @@
 //  Copyright (c) 2014 Ehsan Valizadeh. All rights reserved.
 //
 
+
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[])
-{
+char *read_file_content();
 
-    // insert code here...
-    printf("Hello, World!\n");
+int main(int argc, const char * argv[]) {
+    char *sequence = read_file_content(
+                                       "/Users/ehsan/Documents/Uni/HPC/HW02/HIV-1_db.fasta.txt");
+    char *polymerase = read_file_content(
+                                         "/Users/ehsan/Documents/Uni/HPC/HW02/HIV-1_Polymerase.txt");
+    
+    
+    printf("The HIV sequence is ~> %s", sequence);
+    printf("The HIC polymerase is ~> %s", polymerase);
+    
+    free(sequence);
+    free(polymerase);
     return 0;
 }
 
+char *read_file_content(char* file_path) {
+    long f_size;
+    char *content;
+    size_t code_s;
+    FILE *fp = fopen(file_path, "r");
+    fseek(fp, 0, SEEK_END);
+    f_size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    code_s = sizeof(char) * f_size;
+    content = malloc(code_s);
+    fread(content, 1, f_size, fp);
+    
+    return content;
+}
